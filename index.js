095d121a84db13c60fd24020b382413ed1e4df5f
Karmanya20 <99268305+Karmanya20@users.noreply.github.com>
2023-11-02 20:34:12 +0530

    fourth Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors=require('cors')
-const mysql=require('mysql')
+const mysql=require('mysql2')
 const app = express()
 const port = 5000
 app.use(cors({
@@ -15,6 +15,31 @@ const db=mysql.createConnection({
     password:'Ballislife@10',
     database:'dnk'
 })
+db.connect((err)=>{
+    if(err){
+        console.error("Error");
+        return
+    }
+    console.log("Connection Successful");
+})
+app.post('/signup/buyer', async (req, res) => {
+    try {
+        const {email, password, name}=req.body;
+        db.connect();
+        const rows="INSERT INTO user (email,password,name) VALUES ('"+email+"', '"+password+"','"+name+"' )";
+        db.query(rows,function(error, result){
+            if(error){
+                console.log(error);
+                return
+            }
+            res.send("Account Created Successfully.");
+        })
+        
+    } catch (error) {
+        console.error('Error during signup:', error);
+    res.status(500).json({ message: 'Signup failed' });
+    }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
