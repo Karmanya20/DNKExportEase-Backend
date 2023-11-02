c59bc08075e08091d6e6cc4010516c9d10179793
Karmanya20 <99268305+Karmanya20@users.noreply.github.com>
2023-11-02 16:57:04 +0530

    Third Commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express')
-const sql=require('mysql2')
 const cors=require('cors')
+const mysql=require('mysql')
 const app = express()
 const port = 5000
 app.use(cors({
-    origin:[],
+    origin:["http://localhost:3000"],
     methods:["GET","POST"],
     credentials:true
 }))
 app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Ballislife@10',
+    database:'dnk'
 })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
